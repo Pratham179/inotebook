811972a57c87bc3217541848fd20c919e599f3f1
Pratham Agrawal <prathamagrawal179@gmail.com>
2022-12-23 23:03:35 +0530

    Adding note client side

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
+import Alert from './components/Alert';
 function App() {
   return (
     <>
-    <NoteState>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/"element={<Home/>}></Route>
-          <Route path="/about"element={<About/>}></Route>
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert message="Alert created"/>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+            </Routes>
+          </div>
+        </Router>
       </NoteState>
     </>
   );
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 const NoteState = (props) => {
     const notesInitial = [
         {
-            "_id": "63a462b797d7914d9a8b1350",
+            "_id": "63a462b797d7914d9a88b1350",
             "user": "63a43531f925100f14db9290",
             "title": "My Title",
             "description": "Please wake up early",
@@ -13,7 +13,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "63a46bd24bb66f51db861c1e",
+            "_id": "63a46bd24bb66f51db8661c1e",
             "user": "63a43531f925100f14db9290",
             "title": "My Title",
             "description": "Please wake up early",
@@ -22,19 +22,90 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "63a46bd24bb66f51db861c20",
+            "_id": "63a46bd24bb66f51db8616c20",
             "user": "63a43531f925100f14db9290",
             "title": "My Title",
             "description": "Please wake up early",
             "tag": "personal",
             "date": "2022-12-22T14:38:10.618Z",
             "__v": 0
+        },
+        {
+            "_id": "63a462b797d7914d9a8b13650",
+            "user": "63a43531f925100f14db9290",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2022-12-22T13:59:19.174Z",
+            "__v": 0
+        },
+        {
+            "_id": "63a46bd24bb66f51db8615c1e",
+            "user": "63a43531f925100f14db9290",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2022-12-22T14:38:10.444Z",
+            "__v": 0
+        },
+        {
+            "_id": "63a46bd24bb66f51db8615c20",
+            "user": "63a43531f925100f14db9290",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2022-12-22T14:38:10.618Z",
+            "__v": 0
+        },
+        {
+            "_id": "63a462b797d7914d9a8b13505",
+            "user": "63a43531f925100f14db9290",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2022-12-22T13:59:19.174Z",
+            "__v": 0
+        },
+        {
+            "_id": "63a46bd24bb66f51d5b861c1e",
+            "user": "63a43531f925100f14db9290",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2022-12-22T14:38:10.444Z",
+            "__v": 0
         }
     ]
     const [notes, setNotes] = useState(notesInitial)
 
+
+    // Add a note
+    const addNote = (title,description,tag) => {
+        console.log("Adding a new note")
+        const note={
+            "_id": "63a46bd24bb66f51d5b861c1e",
+            "user": "63a43531f925100f14db9290",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "date": "2022-12-22T14:38:10.444Z",
+            "__v": 0
+        };
+        setNotes(notes.concat(note))
+    }
+
+    // Delete a note
+    const deleteNote = () => {
+
+    }
+
+    // Edit a note
+    const editNote = () => {
+
+    }
+
     return (
-        <NoteContext.Provider value={{ notes, setNotes }}>
+        <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
             {props.children}
         </NoteContext.Provider>
     )
