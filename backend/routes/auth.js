e5c98062ab5faa7941ea9cd79131e7c01d119f7d
Pratham Agrawal <prathamagrawal179@gmail.com>
2022-12-24 23:24:37 +0530

    Added signup component

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,18 +15,19 @@ router.post('/createuser', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
+    let success = false;
     // Finds the validation errors in this request and wraps them in an object with handy functions
     // If there are errors , return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ success, errors: errors.array() });
     }
     // check whether the user with this email exist already
     try {
         let user;
         user = await User.findOne({ email: req.body.email });
         if (user) {
-            return res.status(400).json({ error: "A User with this email already exists" })
+            return res.status(400).json({ success , error: "A User with this email already exists" })
         }
 
         const salt = await bcrypt.genSalt(10);
@@ -46,7 +47,8 @@ router.post('/createuser', [
         const authtoken = jwt.sign(data, JWT_SECRET);
 
         // res.json(user)
-        res.json({ authtoken })
+        success = true;
+        res.json({ success , authtoken })
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
