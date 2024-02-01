de217072d6bf2bd1b3b916fe09809e0c60783b0b
Pratham Agrawal <prathamagrawal179@gmail.com>
2024-02-01 12:10:58 +0530

    backend deployed

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -8,7 +8,7 @@ const Signup = (props) => {
     const handleSubmit = async (e) => {
         const { name, email, password } = credentials;
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        const response = await fetch("https://backend-inotebook-sj0k.onrender.com/api/auth/createuser", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
